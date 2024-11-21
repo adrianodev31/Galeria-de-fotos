c51fdd9adb7e5a7dbc3f76f851058fb0961d1cfd
Adriano Sales <adrianomac@192.168.0.8>
2024-11-20 20:56:52 -0400

    feito deploy

--- src/app/components/ImagemGrande.tsx
+++ src/app/components/ImagemGrande.tsx
@@ -4,14 +4,14 @@ import { Fotos } from "./Fotos"
 
 type Props = {
     img: number;
-    callBack: boolean;
-    callBackAlterandoFoto: ()=> void;
+    callBack: any;
+    callBackAlterandoFoto: any;
 }
 
 export const ImagemGrande = ({img, callBack, callBackAlterandoFoto}: Props) => {
     return (
        <div className="w-screen h-screen top-0 bg-black absolute p-20 flex justify-center items-center">
-            <div className="text-3xl text-green-800 font-bold bg-red-50 p-3 z-10 w-16 h-16 rounded-full cursor-pointer absolute top-3 flex justify-center items-center" onClick={() => callBack(false)}>X</div>
+            <div className="text-3xl text-green-800 font-bold bg-red-50 p-3 z-10 w-16 h-16 rounded-full cursor-pointer absolute top-3 flex justify-center items-center" onClick={() => callBack()}>X</div>
             <img src={Fotos[img].img} className="rounded-xl absolute top-0 w-3/4" alt="" />
             <button className="bg-red-600 p-4 w-24 h-24 rounded-full absolute bottom-2 flex justify-center items-center" onClick={() => callBackAlterandoFoto()}>Mudar foto</button>
        </div>
