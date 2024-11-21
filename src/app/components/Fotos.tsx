64e372ef93455748d1c59d8bb7d799bc68c10774
Adriano Sales <adrianomac@192.168.0.8>
2024-11-20 20:47:04 -0400

    first commit

---
+++ src/app/components/Fotos.tsx
@@ -0,0 +1,14 @@
+export const Fotos = [
+    {id: 1, img: "/1.jpg"},
+    {id: 2, img: "/2.jpg"},
+    {id: 3, img: "/3.jpg"},
+    {id: 4, img: "/4.jpg"},
+    {id: 5, img: "/5.jpg"},
+    {id: 6, img: "/6.jpg"},
+    {id: 7, img: "/7.jpg"},
+    {id: 8, img: "/8.jpg"},
+    {id: 9, img: "/9.jpg"},
+    {id: 10, img: "/10.jpg"},
+    {id: 11, img: "/11.jpg"},
+    {id: 12, img: "/12.jpg"},
+  ]
---
+++ src/app/components/ImagemGrande.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import { Fotos } from "./Fotos"
+
+type Props = {
+    img: number;
+    callBack: boolean;
+    callBackAlterandoFoto: ()=> void;
+}
+
+export const ImagemGrande = ({img, callBack, callBackAlterandoFoto}: Props) => {
+    return (
+       <div className="w-screen h-screen top-0 bg-black absolute p-20 flex justify-center items-center">
+            <div className="text-3xl text-green-800 font-bold bg-red-50 p-3 z-10 w-16 h-16 rounded-full cursor-pointer absolute top-3 flex justify-center items-center" onClick={() => callBack(false)}>X</div>
+            <img src={Fotos[img].img} className="rounded-xl absolute top-0 w-3/4" alt="" />
+            <button className="bg-red-600 p-4 w-24 h-24 rounded-full absolute bottom-2 flex justify-center items-center" onClick={() => callBackAlterandoFoto()}>Mudar foto</button>
+       </div>
+    )
+}
